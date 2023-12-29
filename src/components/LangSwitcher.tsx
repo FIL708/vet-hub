7e0b5cff27a98fac976ccb36886ba1dd42ab7bea
FIL708 <filip.zebrowski4013@gmail.com>
2023-12-29 16:59:29 +0100

    change pl flag icon

--- src/components/LangSwitcher.tsx
+++ src/components/LangSwitcher.tsx
@@ -1,22 +1,32 @@
 import Globe from '@/assets/globe.svg';
 import Arrow from '@/assets/arrow.svg';
+import EnFlag from '@/assets/en-flag.svg';
+import PlFlag from '@/assets/pl-flag.svg';
 
 export default function LangSwitcher() {
     return (
         <div className='dropdown dropdown-end'>
-            <div tabIndex={0} role='button' className='flex items-center p-1'>
+            <div
+                tabIndex={0}
+                role='button'
+                className='flex items-center rounded-2xl border-2 border-transparent p-1 transition hover:border-primary hover:bg-primary-content active:scale-95'
+            >
                 <Globe className='fill-neutral' />
                 <Arrow className='fill-secondary' />
             </div>
             <ul
                 tabIndex={0}
-                className='menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2'
+                className='menu dropdown-content z-[1] rounded-box bg-base-100 p-2'
             >
                 <li>
-                    <a>Item 1</a>
+                    <a>
+                        <PlFlag /> Polski
+                    </a>
                 </li>
                 <li>
-                    <a>Item 2</a>
+                    <a>
+                        <EnFlag /> Angielski
+                    </a>
                 </li>
             </ul>
         </div>
