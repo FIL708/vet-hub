9f16d3fecda46e84d0df3fd4daea779184629b5b
FIL708 <filip.zebrowski4013@gmail.com>
2023-12-30 21:49:17 +0100

    add content to sidebar

--- src/components/DrawerToggle.tsx
+++ src/components/DrawerToggle.tsx
@@ -5,14 +5,12 @@ export default function DrawerToggle({ type }: { type: 'open' | 'close' }) {
     const Icon = type === 'open' ? <Hamburger /> : <Close />;
 
     return (
-        <div className='flex-none lg:hidden'>
-            <label
-                htmlFor='my-drawer-3'
-                aria-label={`${type} sidebar`}
-                className='btn btn-circle border-transparent bg-primary-content hover:border-secondary-content hover:bg-secondary-content lg:hidden'
-            >
-                {Icon}
-            </label>
-        </div>
+        <label
+            htmlFor='drawer'
+            aria-label={`${type} sidebar`}
+            className='btn btn-circle border-transparent bg-transparent hover:border-secondary-content hover:bg-secondary-content lg:hidden'
+        >
+            {Icon}
+        </label>
     );
 }
--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,15 +1,42 @@
+import Link from 'next/link';
 import DrawerToggle from './DrawerToggle';
 
 export default function MobileNavbar() {
+    const links = [
+        { href: '/events', title: 'Warsztaty' },
+        { href: '/blog', title: 'Blog' },
+        { href: '/gallery', title: 'Galeria' },
+        { href: '/about', title: 'O nas' },
+        { href: '/contact', title: 'Kontakt' },
+    ];
+
+    const linkList = links.map((link) => (
+        <li key={link.title}>
+            <Link href={link.href}>{link.title}</Link>
+        </li>
+    ));
+
     return (
         <ul className='menu min-h-full w-80 bg-base-100 p-4'>
-            <DrawerToggle type='close' />
+            <div className='ml-auto'>
+                <DrawerToggle type='close' />
+            </div>
+            <h2 className='my-2 text-center text-xl'>Menu</h2>
             <li>
-                <a>Sidebar Item 1</a>
+                <Link href='/products'>Produkty</Link>
+                <ul>
+                    <li>
+                        <Link href='/products'>Biżuteria</Link>
+                    </li>
+                    <li>
+                        <Link href='/products'>Tkactwo</Link>
+                    </li>
+                    <li>
+                        <Link href='/products'>Stolarstwo</Link>
+                    </li>
+                </ul>
             </li>
-            <li>
-                <a>Sidebar Item 2</a>
-            </li>
+            {linkList}
         </ul>
     );
 }
