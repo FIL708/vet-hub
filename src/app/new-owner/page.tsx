2d378c287a9dde3354b223ebb1c8c3f00c427a7b
FIL708 <filip.zebrowski4013@gmail.com>
2024-01-05 20:21:14 +0100

    add some pages

---
+++ src/app/new-owner/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'VetHub - Nowy właściciel',
+};
+
+export default function NewOwnerPage() {
+    return <div>New Owner Page</div>;
+}
---
+++ src/app/new-pet/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'VetHub - Nowy zwierzak',
+};
+
+export default function NewPetPage() {
+    return <div>New Pet</div>;
+}
