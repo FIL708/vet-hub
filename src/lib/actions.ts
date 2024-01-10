d7014b269a9094ca35cdea19de4a221be872c870
FIL708 <filip.zebrowski4013@gmail.com>
2024-01-10 02:48:21 +0100

    connect new pets and owners with author

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,17 +1,26 @@
 'use server';
 
 import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 import { prisma } from './db/prisma';
-
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { OwnerFormData, PetFormData } from '@/types';
 
 export async function addOwner(formData: OwnerFormData) {
+    const session = await getServerSession(authOptions);
     const { firstName, lastName, redirected } = formData;
+
     if (!firstName || !lastName) {
         throw Error('Missing required fields');
     }
 
-    await prisma.owner.create({ data: { firstName, lastName } });
+    if (!session) {
+        throw Error('Authentication Required');
+    }
+
+    await prisma.owner.create({
+        data: { firstName, lastName, authorId: session.user.id },
+    });
 
     if (redirected) {
         redirect('/');
@@ -19,13 +28,20 @@ export async function addOwner(formData: OwnerFormData) {
 }
 
 export async function addPet(formData: PetFormData) {
+    const session = await getServerSession(authOptions);
     const { name, species, redirected } = formData;
 
     if (!name || !species) {
         throw Error('Missing required fields');
     }
 
-    await prisma.pet.create({ data: { name, species } });
+    if (!session) {
+        throw Error('Authentication Required');
+    }
+
+    await prisma.pet.create({
+        data: { name, species, authorId: session.user.id },
+    });
 
     if (redirected) {
         redirect('/');
